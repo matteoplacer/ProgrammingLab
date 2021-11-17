0656038d88c619a2130b4e4ee5ffac6c9d7cd26f
Matteo Placer <94189731+matteoplacer@users.noreply.github.com>
2021-11-17 17:30:27 +0000

    Funzione che sommi tutti gli elementi di una lista

--- Lezione2.py
+++ Lezione2.py
@@ -1,2 +1,11 @@
-def sum_list (the_list):
-    pass
+lista_numeri = [1, 2, 3, 4, 5, 6]
+
+def somma_lista (lista_numeri):
+    risultato=0
+    for item in lista_numeri:
+        risultato=risultato+item
+    print('Risultato: {}'.format(risultato))
+
+
+risultato=somma(lista_numeri)
+    
